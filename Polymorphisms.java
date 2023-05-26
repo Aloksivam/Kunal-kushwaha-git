516454e281569c1b601a08e4f3dc0bd6105b7d60
Alok <aloksivam@gmail.com>
2023-05-26 10:50:52 +0530

    names.txt file added

---
+++ Polymorphisms.java
@@ -0,0 +1,23 @@
+package JavaWithKunal;
+class Access{
+    int a;
+    int b;
+
+    public Access(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
+public class Polymorphisms {
+    public static void main(String[] args) {
+        System.out.println("alok");
+    }
+}
