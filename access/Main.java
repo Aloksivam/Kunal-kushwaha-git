e7aaa201341071ff3ec84ed1533ac35c42a9d287
Alok <aloksivam@gmail.com>
2023-06-08 14:50:10 +0530

    by ALok

---
+++ access/Main.java
@@ -0,0 +1,12 @@
+package JavaWithKunal.access;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(33,"alok");
+        //
+        ArrayList <Integer> list =  new ArrayList<>();
+        obj.getNum();
+    }
+}
